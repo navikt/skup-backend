958808d70e39c8874ebf1e04e782ce4eaff1d8dd
EIlif Johansen <eilifjohansen@outlook.com>
2025-01-15 18:26:51 +0100

    Tok bort app owner fra dev

--- app/routers/devapps.py
+++ app/routers/devapps.py
@@ -19,7 +19,6 @@ async def add_app(apps: AppsModel, background_tasks: BackgroundTasks, db: Sessio
 
     new_app = Apps(
         app_name=apps.app_name,
-        app_owner="Testbruker",
         created_at=datetime.utcnow().replace(microsecond=0)
     )
     try:
@@ -77,7 +76,7 @@ async def update_app(app_id: UUID4, app_update: AppsUpdateModel, db: Session = D
 
         if app_update.app_name is not None:
             app_to_update.app_name = app_update.app_name
-"""         if app_update.app_owner is not None: 
+"""         if app_update.app_owner is not None:
             app_to_update.app_owner = app_update.app_owner """
         if app_update.is_active is not None:
             app_to_update.is_active = app_update.is_active
