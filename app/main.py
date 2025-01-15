59beb429b4ed64c5b436ef617d0fe583d7bbf132
EIlif Johansen <eilifjohansen@outlook.com>
2025-01-15 14:06:56 +0100

    Tok bort logging

--- app/main.py
+++ app/main.py
@@ -9,12 +9,7 @@ app = FastAPI()
 
 @app.middleware("http")
 async def log_requests(request: Request, call_next):
-    logger.info(f"Incoming request: {request.method} {request.url}")
     auth_header = request.headers.get("Authorization")
-    if auth_header:
-        logger.info(f"Found Authz")
-    else:
-        logger.info("No Authz")
     response = await call_next(request)
     return response
 
